225ddc915da9bba0a2d48cc075826b412825232d
lelevietnam99 <lelevietnam99@gmail.com>
2026-03-19 18:31:36 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'ghi-am-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Cài đặt Service Worker và lưu trữ các file tĩnh
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Đã lưu cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Phục vụ nội dung từ cache khi offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // Trả về file từ cache nếu có
+        }
+        return fetch(event.request); // Nếu không có thì tải từ mạng
+      })
+  );
+});
